c4b4897adf9277bfe8799d3a498adcd104095f66
Ankitjigna <ankitjigna01@gmail.com>
2024-10-01 02:03:24 +0530

    just checking authentcation and hashing, salt for password

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
 const flash = require('connect-flash');
+const passport = require("passport")
+const LocalStrategy = require("passport-local");
+const User = require('./models/user.js');
+
 
 const listings = require("./routes/listing.js");
 const reviews = require("./routes/review.js");
@@ -46,11 +50,28 @@ const sessionOptions = {
 app.use(session(sessionOptions));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req,res,next)=>{
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     next();
 })
+
+app.get("/demouser",async(req,res)=>{
+    let fakeUser = new User({
+        email: "abc@gamil.com",
+        username: "abc",
+    });
+
+    let registeredUser = await User.register(fakeUser,"abc@1234");
+    res.send(registeredUser);
+});
 
 app.get("/",(req,res)=>{
     res.send("home");
