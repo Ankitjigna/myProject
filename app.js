1022cb9dc3c56037ebc8c2520445124cddf1ed0f
Ankitjigna <ankitjigna01@gmail.com>
2024-10-01 02:50:11 +0530

    adding signUp page

--- app.js
+++ app.js
@@ -12,8 +12,10 @@ const LocalStrategy = require("passport-local");
 const User = require('./models/user.js');
 
 
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
+
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
@@ -77,9 +79,9 @@ app.get("/",(req,res)=>{
     res.send("home");
 });
 
-app.use("/listings", listings);
-app.use("/listings/:id/reviews", reviews);
-
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/",userRouter);
 
 app.all("*",(req,res,next)=>{
     next(new ExpressError(404, "page not found!"));
