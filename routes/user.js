a315ac8ce4940cebbcd273523f5c5ef65daafe06
Ankitjigna <ankitjigna01@gmail.com>
2024-10-04 19:01:16 +0530

    modifying login

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require("../models/user.js");
 const passport = require("passport");
 const wrapAsync = require("../utils/wrapAsync.js");
+const {saveRedirectUrl} = require("../middleware.js");
 
 router.get("/signup", (req, res) => {
   res.render("users/signup.ejs");
@@ -16,8 +17,8 @@ router.post(
       const newUser = new User({ username, email });
       let registeredUser = await User.register(newUser, "abc@1234");
       console.log(registeredUser);
-      req.login(registeredUser,(err)=>{
-        if(err){
+      req.login(registeredUser, (err) => {
+        if (err) {
           return next(err);
         }
         req.flash("success", "welcome to duniya!");
@@ -36,21 +37,24 @@ router.get("/login", (req, res) => {
 
 router.post(
   "/login",
+  saveRedirectUrl,
   passport.authenticate("local", {
     failureRedirect: "/login",
     failureFlash: true,
   }),
   async (req, res) => {
-    res.send("welcome, u are logged in!");
+    // res.send("welcome, u are logged in!");
+    req.flash("success", "welcome back to the world!");
+    req.redirect(req.locals.redirectUrl);
   }
 );
 
-router.get("/logout",(req,res,next)=>{
-  req.logOut((err)=>{
-    if (err){
+router.get("/logout", (req, res, next) => {
+  req.logOut((err) => {
+    if (err) {
       next(err);
     }
-    req.flash("success","you are logged out!");
+    req.flash("success", "you are logged out!");
     res.redirect("/listings");
   });
 });
